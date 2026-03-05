dce0a9cb73b114340c3a6c65aa20c19c552c7356
Ubuntu <ubuntu@ip-172-31-42-196.eu-north-1.compute.internal>
2026-03-05 18:02:14 +0000

    Fix: Remove password hashed cast to avoid double hashing

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,7 +28,6 @@ class User extends Authenticatable
 
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
 
     public function employee(): BelongsTo
